7f43e727f547d82dfff27936386eece65eba9411
wardhan2808 <98142605+wardhan2808@users.noreply.github.com>
2022-02-15 15:02:30 +0530

    First Commit

---
+++ Assignment7.py
@@ -0,0 +1,52 @@
+#BT19ECE004 YASHWARDHAN PRATAP SINGH
+#Assignment7
+#Bit SLicing on an Image
+
+#importing required libraries
+import numpy as np 
+import cv2
+import matplotlib.pyplot as plt
+#Reading an Image
+img = cv2.imread(r"C:\Users\yashw\Desktop\images\yashwardhan.png",0)
+
+#Calculating Peak Signal to Noise Ratio (psnr)
+psnr=10*np.log10((255*255)/(1/(225*225)*np.sum(img)*np.sum(img)))
+
+#code for bit slicing
+def cov_binary(num):
+    binary_num = [int(i) for i in list('{0:0b}'.format(num))]
+    for j in range(8 - len(binary_num)):
+        binary_num.insert(0,0)        
+    return binary_num
+
+def conv_decimal(listt):
+    x = 0
+    for i in range(8):
+        x = x + int(listt[i])*(2**(7-i))
+    return x
+
+def discriminate_bit(bit,img):
+    z = np.zeros([225,225])
+    for i in range(225):
+        for j in range(225):
+            x = cov_binary(img[i][j])
+            for k in range(8):
+                if k == bit:
+                    x[k] = x[k]
+                else:
+                    x[k] = 0
+            x1 = conv_decimal(x)
+            z[i][j] = x1
+    return z
+
+# set up side-by-side image display
+fig = plt.figure()
+fig.set_figheight(15)
+fig.set_figwidth(15)
+
+#Dispalying the output
+for i in range(1,9):
+    fig.add_subplot(4,2,i)
+    plt.imshow(discriminate_bit(i-1,img), cmap='gray')
+
+plt.show(block=True)
